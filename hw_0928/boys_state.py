cb230c0f84a805a51ac8f8506e03ccb781364fef
Kclient <go4521304@gmail.com>
2018-10-02 03:28:04 +0900

    hw_0928 완성 / 리소스 추가 (로고, 타이플)

--- hw_0928/boys_state.py
+++ hw_0928/boys_state.py
@@ -1,4 +1,6 @@
 from pico2d import *
+import game_framework
+
 import random
 import math
 
@@ -15,57 +17,74 @@ class Boy:
         self.x = random.randint(0, 800 - 1)
         self.y = random.randint(90, 200)
         self.frame = random.randint(0, 7)
-        self.speed = random.randint(5 ,10)
+        self.speed = random.randint(2 ,5)
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
     def image_update(self):
-        global way_x, way_y
+        global way
 
-        self.degree = math.atan2(float(way_y) - float(self.y), float(way_x) - float(self.x))
+        self.degree = math.atan2(float(way[1]) - float(self.y), float(way[0]) - float(self.x))
 
         self.x += math.cos(self.degree) * self.speed
         self.y += math.sin(self.degree) * self.speed
-
         self.frame = (self.frame + 1) % 8
 
+grass = None
+boys = []
+way = []
 
-def handle_get():
-    global running
-    global way_x, way_y
+def handle_events():
+    global way
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            game_framework.quit()
 
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
-                running = False
+                game_framework.pop_state()
 
         elif event.type == SDL_MOUSEMOTION:
-            way_x, way_y = event.x, 600 - 1 - event.y
+            way = [event.x, 600 - 1 - event.y]
 
-open_canvas(800, 600)
-grass = Grass()
-boys = [Boy() for i in range(20)]
+def enter():
+    global grass
+    global boys
+    global way
 
-running = True
-way_x, way_y = 0, 0
+    grass = Grass()
+    boys = [Boy() for i in range(20)]
+    way = [0, 0]
 
-while(running):
-    handle_get()
+
+def draw():
+    global grass
+    global boys
 
     clear_canvas()
+
     grass.draw()
     for i in boys:
         i.draw()
 
     update_canvas()
 
+
+def update():
+    global boys
     for i in boys:
         i.image_update()
+
     delay(0.03)
 
-close_canvas()
+def exit():
+    global boys
+    global grass
+    global way
+
+    del(boys)
+    del(grass)
+    del(way)
---
+++ hw_0928/main.py
@@ -0,0 +1,5 @@
+import game_framework
+
+import logo_state
+
+game_framework.run(logo_state)
