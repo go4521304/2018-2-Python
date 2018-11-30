957fae35f4af9ea1868a154131f4b82184259a3a
Kclient-Laptop <go4521304@gmail.com>
2018-12-01 03:39:32 +0900

    swing 방식 변경

--- term/main.py
+++ term/main.py
@@ -2,6 +2,6 @@ import game_framework
 import logo_state
 from pico2d import *
 
-open_canvas(1280, 720)
+open_canvas(1280, 720,True)
 game_framework.run(logo_state)
 close_canvas()
--- term/main_state.py
+++ term/main_state.py
@@ -9,33 +9,19 @@ class Player:
 		if (Player.image == None):
 			Player.image = load_image("../res_term/character_1.png")
 		
-		self.gravity = 9.8
-		self.pos = [240, 720 - 100]
-		self.way = 1
+		self.way = 0
 		self.angle_max = 0.0
 		self.angle = 0.0
 		self.velocity = 0.0
-		self.velocity_angle = 0.0
-		self.radius = 100
+		self.radius = 200
+		self.pos = [240, 720 - self.radius]
 
 	def update(self):
-		if self.angle_max == 0:
+		if (self.way == 0):
 			return
 
-		self.velocity = math.sqrt(2 * self.gravity * self.radius * (math.cos(self.angle) - math.cos(self.angle_max)))
-		self.velocity_angle = self.velocity / self.radius
 
-		self.pos = [240 + (self.radius * math.sin(self.angle)), 720 - (self.radius * math.cos(self.angle))]
 
-		self.angle = self.angle + (self.velocity_angle * self.way * game_framework.frame_time)
-
-		if (self.angle < self.angle_max or self.angle > self.angle_max):
-			self.angle_max = self.angle_max * -1 / 2
-			if (abs(self.angle_max) < 1 * math.pi):
-				self.angle_max = 0.0
-				self.angle = 0.0
-				self.velocity = 0.0
-				self.velocity_angle = 0.0
 
 	def draw(self):
 		clear_canvas()
@@ -44,7 +30,7 @@ class Player:
 	
 def enter():
 	global player
-	player = Player
+	player = Player()
 
 def exit():
 	global player
@@ -57,15 +43,16 @@ def resume():
 	pass
 
 def handle_events():
-    events = get_events()
-    for e in events:
-        if (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
-            game_framework.pop_state()
+	global player
+	events = get_events()
+	for e in events:
+		if (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+			game_framework.pop_state()
 
 def update():
 	global player
-	player.update
+	player.update()
 
 def draw():
 	global player
-	player.draw
+	player.draw()
