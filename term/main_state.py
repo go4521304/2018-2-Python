f0d235d2aaeb853aba09390f11a364dd008a7546
Kclient-Laptop <go4521304@gmail.com>
2018-12-14 13:12:49 +0900

    scroll...
    scroll ing....

--- term/main_state.py
+++ term/main_state.py
@@ -5,6 +5,7 @@ import time
 import json
 
 class Obj:
+	WH = [10, 100]
 	def __init__(self):
 		self.stage = '0'
 		self.num = '0'
@@ -17,19 +18,29 @@ class Player:
 	STAGE_NUM = None
 
 	image = None
+	image_back = None
+	image_rope = None
 	WH = [120, 160]
 	state = ['Sleep', 'Click', 'Swing', 'Jump']
 	pos_center = [320, 720] 
 	
 	def __init__(self):
+		global rope
 		if (Player.image == None):
 			Player.image = load_image("../res_term/character_1.png")
+		if (Player.image_back == None):
+			Player.image_back = [load_image("../res_term/Stage_1_B.png"), load_image("../res_term/Stage_2_B.png")\
+			, load_image("../res_term/Stage_3_B.png"), load_image("../res_term/Stage_4_B.png")]
+		if (Player.image_rope == None):
+			Player.image_rope = load_image("../res_term/Rope.png")
 		
 		if (Player.STAGE_NUM == None):
 			Player.STAGE_NUM = 0
+
+		self.rope_num = 0
 		
 		self.cur_state = Player.state[0]
-		self.radious = 0
+		self.radious = rope[Player.STAGE_NUM].length[self.rope_num]
 
 		self.pos = [320, 720 - self.radious]
 		self.t_pos = [0, 0]
@@ -55,6 +66,27 @@ class Player:
 		# Jump
 		elif key == Player.state[3]:
 			self.cur_state = player.state[3]
+
+	# type == 0 / rope
+	def check_Boundary(self, Target, type):
+		if (type == 0):
+			if (self.pos[0] + self.WH[0] > 960 and self.pos[0] - self.WH[0] < 960) and\
+			 (self.pos[1] - self.WH[1] < Target * 100 and self.pos[1] + self.WH[1] > (Target + 1) * 100):
+			 return True 
+
+	def scrolling(self):
+		
+
+	def reset(self):
+		self.rope_num += 1
+		self.radious = rope[Player.STAGE_NUM].length[self.rope_num]
+		self.pos = [320, 720 - self.radious]
+		self.t_pos = [0, 0]
+		self.way = 0
+		self.max_angle = 0.0
+		self.angle = 0.0
+		self.velocity = 0.0
+		self.fram_count = 0
 
 	def update(self):
 		# Sleep
@@ -86,7 +118,7 @@ class Player:
 			if (abs(self.angle) > abs(self.max_angle)):
 				self.angle = abs(self.max_angle) * (self.way)
 
-			self.velocity = math.sqrt(2 * 9.8 * self.radious / 100 * (math.cos(self.angle) - math.cos(self.max_angle)))
+			self.velocity = math.sqrt(2 * 9.8 * self.radious / 50 * (math.cos(self.angle) - math.cos(self.max_angle)))
 			print(self.velocity, self.angle)
 			self.angle = self.angle + (self.velocity / self.radious * self.way)
 
@@ -94,8 +126,16 @@ class Player:
 
 		# Jump
 		elif self.cur_state == player.state[3]:
-			self.pos[0] = self.pos[0] + (self.velocity * math.cos(self.angle)) * 5
+			self.pos[0] = self.pos[0] + (self.velocity * math.cos(self.angle)) * 3
 			self.pos[1] = self.pos[1] + ((self.velocity * math.sin(self.angle) - (9.8 * (time.time() - self.fram_count)/2)))
+
+			for i in range ((int)(rope[self.STAGE_NUM].length[self.rope_num]/100)):
+				if (self.check_Boundary(i, 0) == True):
+					self.change_state('Sleep')
+					self.scrolling()
+					self.reset()
+					return
+
 
 			#if self.pos[1] > 620:
 			#	game_framework.change_state(game_over)
@@ -103,7 +143,16 @@ class Player:
 
 
 	def draw(self):
+		global rope
 		clear_canvas()
+		player.image_back[self.STAGE_NUM].clip_draw(0, 0, 1280, 720, 1280/2, 720/2)
+		for i in range((int)(rope[self.STAGE_NUM].length[self.rope_num]/100)):
+			Player.image_rope.clip_composite_draw(0, 0, 35, 100, self.angle, 'None',\
+			 Player.pos_center[0] + (math.sin(self.angle) * i * 100), 720 - (math.cos(self.angle) * i * 100), 10, 100)
+
+		for i in range((int)(rope[self.STAGE_NUM].length[self.rope_num + 1]/100)):
+			Player.image_rope.clip_draw(0, 0, 35, 100, 960, 720 - (i * 100), 10, 100)
+
 		Player.image.clip_draw(0, 0, 120, 160, self.pos[0], self.pos[1])
 		update_canvas()
 	
@@ -111,7 +160,6 @@ class Player:
 def enter():
 	global player
 	global rope
-	player = Player()
 	rope = []
 
 	fh = open('obj_date.json', 'r')
@@ -125,6 +173,8 @@ def enter():
 		St.monster = e['monster']
 		St.monster_type = e['monster_type']
 		rope.append(St)
+	
+	player = Player()
 
 
 def exit():
